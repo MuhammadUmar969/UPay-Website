bead1fd8e1773012ea59c04bc216a9ab3271d10d
MuhammadUmar969 <muhammadumarazizkhan23@gmail.com>
2023-06-06 00:14:01 +0500

    AboutUs

--- src/HomeWorks.jsx
+++ src/HomeWorks.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import workApi from "./Api/Work";
 
-const HomeWorks = () => {
+const HowItWorks = () => {
+  const [workData, setWorkData] = useState(workApi);
+ 
   return (
     <>
       <section>
         <div className="work-container container">
-            <h1 className="main-heading text-center">
-                How does it works
-            </h1>
-
-            <div className="row">
-                <div className="col-12 col-lg-6 text-center">
-                    
-                </div>
-            </div>
+          <h1 className="main-heading text-center">How does it Work</h1>
+          <div className="row">
+            {workData.map((curElem) => {
+              const { id, logo, title, info } = curElem;
+              return (
+                <>
+                  <div className="col-12 col-lg-4 text-center work-container-subdiv">
+                    <i class={`fontawesome-style ${logo}`}></i>
+                    <h2 className="sub-heading">{title}</h2>
+                    <p className="main-hero-para w-100">{info}</p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default HomeWorks
+export default HowItWorks;
